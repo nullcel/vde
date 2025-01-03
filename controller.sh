ebc37109212360436bd46f02215d35fd5cfaa1d5
stuxtwo <dimaac@bk.ru>
2025-01-03 14:24:58 +0100

    Added controller.sh, modified gpuusage.sh

---
+++ controller.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Function for displaying the prompt and handling commands
+while true; do
+    echo -n "> "
+    read cmd 
+
+    case $cmd in
+        fetch)
+            echo "Fetching latest changes from git..."
+            git fetch
+            ;;
+        up)
+            echo "Starting Docker containers with docker-compose up..."
+            docker-compose up
+            ;;
+        build)
+            echo "Starting Docker containers with docker-compose up --build..."
+            docker-compose up --build
+            ;;
+        
+        # Command to pull the latest changes from git and docker-compose
+        pull)
+            echo "Pulling latest changes from git and docker-compose..."
+            git pull
+            docker-compose pull
+            ;;
+        
+        # Command to bring down Docker containers
+        down)
+            echo "Stopping Docker containers with docker-compose down..."
+            docker-compose down
+            ;;
+        
+        # Command to exit the script
+        exit)
+            echo "Exiting..."
+            break
+            ;;
+        
+        # Invalid command
+        *)
+            echo "Invalid command. Available commands: fetch, down, up, pull, build, exit"
+            ;;
+    esac
+done
--- gpuusage.sh
+++ gpuusage.sh
@@ -1,1 +1,4 @@
+#!/bash/bin
+
+echo "If you have an error, make sure you run \"./controller.sh up\" before opening an issue."
 docker exec -it vde python -c "import torch; print(torch.cuda.is_available())"
